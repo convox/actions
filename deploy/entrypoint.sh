f84ef615c3890fa06f7fb1eb8a2807c584c831c6
camerongray <868953+camerondgray@users.noreply.github.com>
2019-09-12 14:53:16 -0700

    moving some env variable to input parameters

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -4,7 +4,7 @@ then
     echo "Convox password must be set"
 else
     echo "Deploying"
-    export $CONVOX_HOST = $INPUT_CONVOX_HOST
-    export $CONVOX_RACK = $INPUT_CONVOX_RACK
+    export CONVOX_HOST = $INPUT_CONVOX_HOST
+    export CONVOX_RACK = $INPUT_CONVOX_RACK
     convox deploy --app $INPUT_CONVOX_APP
 fi
