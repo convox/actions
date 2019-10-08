d1781361b87db1480547d95112606f8c68126608
camerongray <868953+camerondgray@users.noreply.github.com>
2019-10-08 16:17:41 -0700

    initial pass at multi-step build promote workflows

---
+++ build/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+if [ -z "$CONVOX_PASSWORD"]
+then
+    echo "Convox password must be set"
+else
+    echo "Building"
+    export CONVOX_HOST=$INPUT_CONVOX_HOST
+    export CONVOX_RACK=$INPUT_CONVOX_RACK
+    release_id=$(convox build --app $INPUT_CONVOX_APP --id)
+    echo :: set-output name=release_id::$release_id
+fi
---
+++ promote/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if [ -z "$CONVOX_PASSWORD"]
+then
+    echo "Convox password must be set"
+else
+    echo "Building"
+    export CONVOX_HOST=$INPUT_CONVOX_HOST
+    export CONVOX_RACK=$INPUT_CONVOX_RACK
+    convox promote --app $INPUT_CONVOX_APP --release=$INPUT_RELEASE_ID
+fi
