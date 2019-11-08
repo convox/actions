9cf7faf010faa372a6373043b052f39d7975ab67
camerongray <868953+camerondgray@users.noreply.github.com>
2019-11-07 17:45:34 -0800

    initial support for run

---
+++ run/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if [ -z "$CONVOX_PASSWORD"]
+then
+    echo "Convox password must be set"
+else
+    echo "Building"
+    export CONVOX_HOST=$INPUT_CONVOX_HOST
+    export CONVOX_RACK=$INPUT_CONVOX_RACK
+    convox run $INPUT_SERVICE $INPUT_COMMAND --release $INPUT_RELEASE_ID --app $INPUT_CONVOX_APP --rack $INPUT_CONVOX_RACK
+fi
