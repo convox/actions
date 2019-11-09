364bbdb6ed3929bd160aceb9d24144a48d379bfb
camerongray <868953+camerondgray@users.noreply.github.com>
2019-11-08 17:24:30 -0800

    cleanup

--- promote/entrypoint.sh
+++ promote/entrypoint.sh
@@ -2,6 +2,6 @@
 
 echo "Promoting Release $INPUT_RELEASE_ID"
 export CONVOX_PASSWORD=$INPUT_PASSWORD
-export CONVOX_HOST=$INPUT_CONVOX_HOST
-export CONVOX_RACK=$INPUT_CONVOX_RACK
-convox releases promote $INPUT_RELEASE_ID --app $INPUT_CONVOX_APP
+export CONVOX_HOST=$INPUT_HOST
+export CONVOX_RACK=$INPUT_RACK
+convox releases promote $INPUT_RELEASE_ID --app $INPUT_APP
