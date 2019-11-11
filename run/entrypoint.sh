3dd85321594b6c609fa27d64698475e7f0f2a663
camerongray <868953+camerondgray@users.noreply.github.com>
2019-11-11 13:34:55 -0800

    changing release ID to be set by the build by default

--- run/entrypoint.sh
+++ run/entrypoint.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
-if ["$INPUT_RELEASE"]
+if [ -n "$INPUT_RELEASE" ]
 then
-    export RELEASE=$INPUT_RELEASE
+ export RELEASE=$INPUT_RELEASE
+fi
+if [ -z "$RELEASE" ]
+then
+  echo "Release must either be passed as input or set by running a build step"
 else
-    if [ -z "$RELEASE"]
-    then
-        echo "Release must either be passed as input or set by running a build step"
-    else
-        echo "Running command"
-        export CONVOX_RACK=$INPUT_RACK
-        convox run $INPUT_SERVICE "$INPUT_COMMAND" --release $INPUT_RELEASE --app $INPUT_APP --rack $INPUT_RACK
-    fi
+  echo "Running command"
+  export CONVOX_RACK=$INPUT_RACK
+  convox run $INPUT_SERVICE "$INPUT_COMMAND" --release $INPUT_RELEASE --app $INPUT_APP --rack $INPUT_RACK
 fi
 
