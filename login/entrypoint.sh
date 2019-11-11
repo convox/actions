d5732e53c7d9d676e35b72e80b06f7395aa77f17
camerongray <868953+camerondgray@users.noreply.github.com>
2019-11-11 11:12:04 -0800

    adding a login action and testing with build

--- login/entrypoint.sh
+++ login/entrypoint.sh
@@ -3,4 +3,4 @@ echo "Building"
 export CONVOX_PASSWORD=$INPUT_PASSWORD
 echo ::set-env name=CONVOX_PASSWORD::$INPUT_PASSWORD
 echo ::set-env name=CONVOX_HOST::$INPUT_HOST
-convox login $INPUT_HOST
+convox login $INPUT_HOST --password $INPUT_PASSWORD
