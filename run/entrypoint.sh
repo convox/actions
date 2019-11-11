13e482623b42d0433e853076846044c8f4b231d1
camerongray <868953+camerondgray@users.noreply.github.com>
2019-11-11 13:39:15 -0800

    changing release ID to be set by the build by default

--- run/entrypoint.sh
+++ run/entrypoint.sh
@@ -9,6 +9,6 @@ then
 else
   echo "Running command"
   export CONVOX_RACK=$INPUT_RACK
-  convox run $INPUT_SERVICE "$INPUT_COMMAND" --release $INPUT_RELEASE --app $INPUT_APP --rack $INPUT_RACK
+  convox run $INPUT_SERVICE "$INPUT_COMMAND" --release $RELEASE --app $INPUT_APP --rack $INPUT_RACK
 fi
 
